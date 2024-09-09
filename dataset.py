6c520b12275685be9b0927b75dca65b0912cfeee
LucaLemos <65423512+LucaLemos@users.noreply.github.com>
2024-09-09 10:38:41 -0300

    mudando tudo

--- dataset.py
+++ dataset.py
@@ -3,27 +3,31 @@ import cv2
 import numpy as np
 from torch.utils.data import Dataset
 
-class Dataset(Dataset):
-    def __init__(self, root_dir, transform=None, img_size=(224, 224)):
+class FacialLandmarkDataset(Dataset):
+    def __init__(self, root_dir):
         self.root_dir = root_dir
-        self.image_pairs = self._load_image_pairs()
-        self.transform = transform
-        self.img_size = img_size
+        self.image_files, self.landmark_files = self._load_files()
     
-    def _load_image_pairs(self):
-        image_pairs = []
+    def _load_files(self):
+        image_files = {}
+        landmark_files = {}
+
+        # Loop through the directory and collect image files and their corresponding landmarks
         for filename in os.listdir(self.root_dir):
-            if filename.endswith('_mirror.jpg'):
-                normal_img = filename.replace('_mirror.jpg', '.jpg')
-                pts_file = filename.replace('_mirror.jpg', '.pts')
+            if filename.endswith('.jpg'):
+                base_filename = filename.split('_')[0]  # Keep only the base of the filename (without _1, _2)
+                img_path = os.path.join(self.root_dir, filename)
+                if base_filename not in image_files:
+                    image_files[base_filename] = img_path
                 
-                mirror_img_path = os.path.join(self.root_dir, filename)
-                normal_img_path = os.path.join(self.root_dir, normal_img)
-                pts_path = os.path.join(self.root_dir, pts_file)
-                
-                if os.path.isfile(normal_img_path) and os.path.isfile(pts_path):
-                    image_pairs.append((normal_img_path, mirror_img_path, pts_path))
-        return image_pairs
+            if filename.endswith('.pts'):
+                base_filename = filename.split('_')[0]  # Keep the same base to associate with the image
+                pts_path = os.path.join(self.root_dir, filename)
+                if base_filename not in landmark_files:
+                    landmark_files[base_filename] = []
+                landmark_files[base_filename].append(pts_path)
+
+        return list(image_files.values()), landmark_files
 
     def _load_image(self, img_path):
         img = cv2.imread(img_path)
@@ -31,56 +35,35 @@ class Dataset(Dataset):
             raise ValueError(f"Failed to load image at {img_path}")
         return img
 
-    def _load_landmarks(self, pts_path):
-        with open(pts_path, 'r') as file:
-            lines = file.readlines()
-        
-        points = []
-        reading_points = False
-        
-        for line in lines:
-            line = line.strip()
-            if line.startswith('{'):
-                reading_points = True
-                continue
-            if reading_points:
-                if line.startswith('}'):
-                    break
-                x, y = map(float, line.split())
-                points.append((x, y))
-        
-        return np.array(points)
-    
-    def _resize_image_and_landmarks(self, img, landmarks):
-        original_size = img.shape[:2]  # (height, width)
-        img_resized = cv2.resize(img, self.img_size)
-        
-        # Calculate scaling factors
-        scale_x = self.img_size[0] / original_size[1]  # new width / old width
-        scale_y = self.img_size[1] / original_size[0]  # new height / old height
-        
-        # Scale the landmarks
-        landmarks_resized = landmarks.copy()
-        landmarks_resized[:, 0] *= scale_x  # x-coordinates
-        landmarks_resized[:, 1] *= scale_y  # y-coordinates
-        
-        return img_resized, landmarks_resized
+    def _load_landmarks(self, pts_paths):
+        all_landmarks = []
+        for pts_path in pts_paths:
+            with open(pts_path, 'r') as file:
+                lines = file.readlines()
+            points = []
+            reading_points = False
+            for line in lines:
+                line = line.strip()
+                if line.startswith('{'):
+                    reading_points = True
+                    continue
+                if reading_points:
+                    if line.startswith('}'):
+                        break
+                    x, y = map(float, line.split())
+                    points.append((x, y))
+            all_landmarks.append(np.array(points))
+        return np.array(all_landmarks)
     
     def __len__(self):
-        return len(self.image_pairs)
+        return len(self.image_files)
     
     def __getitem__(self, idx):
-        normal_img_path, mirror_img_path, pts_path = self.image_pairs[idx]
+        img_path = self.image_files[idx]
+        base_filename = os.path.basename(img_path).split('_')[0]
+        pts_paths = self.landmark_files[base_filename]
         
-        normal_img = self._load_image(normal_img_path)
-        mirror_img = self._load_image(mirror_img_path)
-        landmarks = self._load_landmarks(pts_path)
-
-        normal_img, landmarks = self._resize_image_and_landmarks(normal_img, landmarks)
-        mirror_img, _ = self._resize_image_and_landmarks(mirror_img, landmarks)
+        img = self._load_image(img_path)
+        landmarks = self._load_landmarks(pts_paths)
         
-        if self.transform:
-            normal_img = self.transform(normal_img)
-            mirror_img = self.transform(mirror_img)
-        
-        return normal_img, mirror_img, landmarks
+        return img, landmarks
